e239c8623b7c174840cd7b68716b851e46568c5e
xuantinh <TinhNX4@fpt.com>
2025-09-25 08:43:51 +0700

    manager user and dashboard

---
+++ src/components/admin/AdminDashboard.js
@@ -0,0 +1,43 @@
+"use client"
+
+import { useState } from "react"
+import Sidebar from "./SiderBarAdmin"
+import UserManagement from "./ManagerUser"
+import PartnerManagement from "./ManagerPanter"
+import PartnerRequests from "./PartnerRequest"
+import DashAdmin from "./DashAdmin"
+
+const AdminDashboard = () => {
+  const [activeSection, setActiveSection] = useState("dashboard")
+
+  // Render nội dung theo menu sidebar
+  const renderContent = () => {
+    switch (activeSection) {
+      case "users":
+        return <UserManagement />
+      case "partners":
+        return <PartnerManagement />
+      case "partner-requests":
+        return <PartnerRequests />
+      default:
+        return <DashAdmin /> 
+    }
+  }
+
+  return (
+    <div className="admin-dashboard flex">
+      {/* Sidebar */}
+      <Sidebar
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+      />
+
+      {/* Nội dung */}
+      <main className="admin-content flex-1 bg-gray-100 min-h-screen p-6">
+        {renderContent()}
+      </main>
+    </div>
+  )
+}
+
+export default AdminDashboard
